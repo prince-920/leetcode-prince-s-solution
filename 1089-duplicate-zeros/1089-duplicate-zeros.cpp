9db809f9f48ae153bffe211bca1a50582a3ee643
Prince Kumar <prince_2024bmme016@nitsri.ac.in>
2026-07-03 22:43:13 +0530

    Time: 0 ms (100%), Space: 14.5 MB (7.72%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+vector<int>ans;
+    for ( int i = 0 ; i<n ; i++){
+        if ( arr[i] !=0){
+ans.push_back(arr[i]);
+        }
+        else{
+            ans.push_back(0);
+            ans.push_back(0);
+        }
+    }
+    ans.resize(n);
+    arr = ans;
+
+
+    }
+};
