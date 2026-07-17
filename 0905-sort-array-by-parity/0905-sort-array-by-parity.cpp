16534a3058c3318e96e366add886454a9de59133
Prince Kumar <prince_2024bmme016@nitsri.ac.in>
2026-07-17 22:18:48 +0530

    Time: 0 ms (100%), Space: 21.1 MB (6.27%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+
+vector < int> ans1;
+vector<int>ans2;
+
+
+        for ( int i =0 ; i<n ; i++){
+            if (   nums[i]%2==0){
+                ans1.push_back( nums[i]);
+            }
+            else {
+                ans2.push_back( nums[i]);
+            }
+        }
+          for ( int i =0 ; i<ans2.size(); i++){
+            ans1.push_back( ans2[i]);
+        }
+
+return ans1;
+    }
+};
